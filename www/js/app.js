0b4040aca0fc0fd9c758ef2832a4bc32cdcf4831
filwalr <wallerp@gmail.com>
2016-08-08 02:46:54 -0400

    Create  service to pull in Reddit Feed
    Initialize stories to be empty array
    
    note to self: can log responses with console and use chrome developer tool to view them. responses have multiple fields (title, thumbnail, etc:  right now only pulling title into index.htm)

--- www/js/app.js
+++ www/js/app.js
@@ -2,19 +2,16 @@
 
 var app = angular.module('feedreader', ['ionic']);
 
-app.controller('FeedCtrl', function($scope) {
+app.controller('FeedCtrl', function($http, $scope) {
 
-  $scope.stories = [
-    {
-      title:'First Story'
-    },
-    {
-      title:'Second Story'
-    },
-    {
-      title:'Third Story'
-    }
-  ];
+  $scope.stories = [];
+
+  $http.get('https://www.reddit.com/r/bengals/new/.json')
+    .success(function(response) {
+      angular.forEach(response.data.children, function(child) {
+        $scope.stories.push(child.data);
+      });
+    });
 
 });
 
