eed3e41d195801a5967b2e78a9edda55010b0dbc
filwalr <wallerp@gmail.com>
2016-08-08 02:29:36 -0400

    created dynamic story list w/ ng-repeat.  tested by adding thir story to story array inapp.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,24 @@
-(function() { 
-var app = angular.module('starter', ['ionic'])
+(function(){
 
-.run(function($ionicPlatform) {
+var app = angular.module('feedreader', ['ionic']);
+
+app.controller('FeedCtrl', function($scope) {
+
+  $scope.stories = [
+    {
+      title:'First Story'
+    },
+    {
+      title:'Second Story'
+    },
+    {
+      title:'Third Story'
+    }
+  ];
+
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
